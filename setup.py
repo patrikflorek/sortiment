327573427390e4a8370997a1e1db506400d00b5e
Patrik Florek <patrik.florek@gmail.com>
2024-11-18 15:00:50 +0000

    Bump version to 0.1.0dev in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sortiment",
-    version="0.1.0",
+    version="0.1.0dev",
     packages=find_packages(),
     install_requires=[
         "kivy>=2.3.0",
